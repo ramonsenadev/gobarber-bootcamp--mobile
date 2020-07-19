74e31d4f45629453e1aaf0f019e495a628c0cfef
Ramon Sena <ramonsenadev@gmail.com>
2020-07-19 15:48:03 -0300

    Cadastro na aplicação utilizando o axios

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -15,6 +15,8 @@ import * as Yup from 'yup';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
+import api from '../../services/api';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -58,9 +60,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso',
+        'Você já pode fazer login na aplicação.',
+      );
+
+      navigation.goBack();
 
       Alert.alert(
         'Cadastro realizado',
